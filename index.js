0d87d1d32e0afe5cfa6554d41d0aaf167debb103
Sim Greenbaum <gbs4ever2@gmail.com>
2019-04-24 03:12:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,3 @@ function findMatching(driver, string){
 return driver.filter(function (name) { return name.tolowercase() === string.lowercase() ; })
 }
 
-function findMatching (list, name) {
-  return list.filter(function (driverName) {
-    return driverName.toLowerCase() === name.toLowerCase();
-  });
-}
