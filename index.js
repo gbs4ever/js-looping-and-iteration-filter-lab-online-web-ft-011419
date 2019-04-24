54fbfd0b4977cdbab4323e4c8c47dbe9020367b7
Sim Greenbaum <gbs4ever2@gmail.com>
2019-04-24 03:11:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(driver, string){
-driver.filter(function (name) { return name === string ; })
+driver.filter(function (name) { return name.lowercase === string.lowercase ; })
 }
